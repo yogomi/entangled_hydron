2ae0761d886217f4e904269af3a4aea1b0a15de1
Makoto Yano <makoto.yano@soliton.co.jp>
2014-07-08 21:31:57 +0900

    idを符号付き整数へ。
    Vector型とうまくやり取りするため。

--- include/synapse.h
+++ include/synapse.h
@@ -8,11 +8,11 @@
 class SynapseId {
  public:
   SynapseId(): x_(0), y_(0), z_(0) {}
-  SynapseId(uint32_t x, uint32_t y, uint32_t z)
+  SynapseId(int32_t x, int32_t y, int32_t z)
     : x_(x), y_(y), z_(z) {}
   ~SynapseId() {}
  private:
-  uint32_t x_, y_, z_;
+  int32_t x_, y_, z_;
 };
 
 class Synapse {
