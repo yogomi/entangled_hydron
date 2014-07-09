aa17619090fc3601b29b9e9315f1fcf782fb672b
Makoto Yano <makoto.yano@soliton.co.jp>
2014-07-09 19:29:17 +0900

    Hydronの出力を充実。

--- src/hydron.cc
+++ src/hydron.cc
@@ -1,6 +1,8 @@
 // Copyright 2014 Makoto Yano
 
 #include <cstdint>
+#include <list>
+#include <algorithm>
 
 #include "include/hydron.h"
 
@@ -25,7 +27,12 @@ void Hydron::ChangeId(const int32_t x, const int32_t y, const int32_t z) {
 }
 
 void Hydron::ShowStatus() {
-  printf("ID:(%d, %d, %d) ", id_.x(), id_.y(), id_.z());
+  printf("ID: (%d, %d, %d); ", id_.x(), id_.y(), id_.z());
+  printf("threshold: %d; ", threshold_);
+  printf("strength: %d; ", strength_);
+  printf("step: %d; ", step_);
+  printf("refractory period: %d; ", refractory_period_);
+  printf("connectiong hydron count: %lu; ", connecting_hydron_.size());
   printf("\n");
 }
 
