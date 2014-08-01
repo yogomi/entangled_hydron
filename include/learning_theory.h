c88c1755ffce3a1cb38fd6285559cd5506ba0263
Makoto Yano <makoto.yano@soliton.co.jp>
2014-08-01 19:46:41 +0900

    LearningTheoryの抽象基底クラスとFeedLearningクラスを追加

---
+++ include/learning_theory.h
@@ -0,0 +1,36 @@
+// Copyright 2014 Makoto Yano
+
+#ifndef INCLUDE_LEARNING_THEORY_H_
+#define INCLUDE_LEARNING_THEORY_H_
+
+#include <map>
+
+#include "./hydron.h"
+
+namespace hydron {
+
+struct LearningParameter {
+  float food;
+};
+
+class LearningTheory {
+ public:
+  LearningTheory() {}
+  ~LearningTheory() {}
+
+  virtual void Learning(std::map<HydronId, Hydron> &hydron_map
+                      , struct LearningParameter &parameter) = 0;  // NOLINT
+};
+
+class FeedLearning: public LearningTheory {
+ public:
+  FeedLearning() {}
+  ~FeedLearning() {}
+
+  void Learning(std::map<HydronId, Hydron> &hydron_map
+              , struct LearningParameter &parameter);  // NOLINT
+};
+
+}  // namespace hydron
+
+#endif  // INCLUDE_LEARNING_THEORY_H_
---
+++ src/learning_theory.cc
@@ -0,0 +1,11 @@
+// Copyright 2014 Makoto Yano
+
+#include "./learning_theory.h"
+
+namespace hydron {
+
+void FeedLearning::Learning(std::map<HydronId, Hydron> &hydron_map
+              , struct LearningParameter &parameter) {  // NOLINT
+}
+
+}  // namespace hydron
